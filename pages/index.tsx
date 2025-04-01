75cca0ceabe3ecaac0a9ed865ee2249b204f0014
Caleb Bradshaw <therealcalebbradshaw@gmail.com>
2025-03-31 20:34:08 -0600

    minor ui fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -186,7 +186,7 @@ export default function Home() {
     >
       <HeroSection>
         <HeroContent>
-          <TypewriterHeading text="Hello there, I'm Caleb " delay={600} />
+          <TypewriterHeading text="Hello there, I'm Caleb " delay={300} />
           <HeroSubtitle>
             Computer Science student at BYU with a passion for web development, 
             machine learning, and problem solving
