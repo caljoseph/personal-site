6971c3386e12e578388e48e31acbddc36ab30869
Caleb Bradshaw <therealcalebbradshaw@gmail.com>
2025-04-03 00:06:49 -0600

    add seo

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -21,6 +21,7 @@ const MainContent = styled.main`
 `;
 
 const Layout: React.FC<LayoutProps> = ({ children, title = 'Caleb Bradshaw', description = 'Personal website of Caleb Bradshaw' }) => {
+  const url = 'https://calebbradshaw.dev';
   return (
     <AppContainer>
       <Head>
@@ -28,6 +29,21 @@ const Layout: React.FC<LayoutProps> = ({ children, title = 'Caleb Bradshaw', des
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="canonical" href={url} />
+        
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={`${url}/images/profile-pic.jpg`} />
+        
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={url} />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={`${url}/images/profile-pic.jpg`} />
       </Head>
       <Header />
       <MainContent>{children}</MainContent>
